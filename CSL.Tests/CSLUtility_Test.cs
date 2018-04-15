971715775f32adb3162c7e7287b8c1ee27695075
erabbit <rabbitom@gmail.com>
2018-04-15 22:09:46 +0800

    更新测试

--- CSL.Tests/CSLUtility_Test.cs
+++ CSL.Tests/CSLUtility_Test.cs
@@ -7,16 +7,29 @@ namespace CSL.Tests
     public class CSLUtility_Test
     {
         [Fact]
-        public void ToIntLE()
+        public void ToIntLE_Test()
         {
-            var result = CSLUtility.ToIntLE(new byte[] { 1, 2, 3 }, 0, 3);
-            Assert.Equal(0x030201, result);
-
-            result = CSLUtility.ToIntLE(new byte[] { 1, 2, 3 }, 0, 2);
-            Assert.Equal(0x0201, result);
-
-            result = CSLUtility.ToIntLE(new byte[] { 1, 2, 3 }, 1, 2);
-            Assert.Equal(0x0302, result);
+            Assert.Equal(0x030201, CSLUtility.ToIntLE(new byte[] { 1,2,3 }, 0, 3));
+            Assert.Equal(0x0201, CSLUtility.ToIntLE(new byte[] { 1,2,3 }, 0, 2));
+            Assert.Equal(0x0302, CSLUtility.ToIntLE(new byte[] { 1,2,3 }, 1, 2));
         }
+
+        [Fact]
+        public void ToIntBE_Test()
+        {
+            Assert.Equal(0x010203, CSLUtility.ToIntBE(new byte[] { 1,2,3 }, 0, 3));
+            Assert.Equal(0x0102, CSLUtility.ToIntBE(new byte[] { 1,2,3 }, 0, 2));
+            Assert.Equal(0x0203, CSLUtility.ToIntBE(new byte[] { 1,2,3 }, 1, 2));
+        }
+
+        [Fact]
+        public void ToHexString_Test()
+        {
+            Assert.Equal("010AFF", CSLUtility.ToHexString(new byte[] { 1,10,255 }, 0, 3, ""));
+            Assert.Equal("01-0A-FF", CSLUtility.ToHexString(new byte[] { 1,10,255 }, 0, 3, "-"));
+            Assert.Equal("01-0A", CSLUtility.ToHexString(new byte[] { 1,10,255 }, 0, 2, "-"));
+            Assert.Equal("0A-FF", CSLUtility.ToHexString(new byte[] { 1,10,255 }, 1, 2, "-"));
+        }
+
     }
 }
